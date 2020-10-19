f260f4a6d434b0492b020f0862ca3738556fb32a
Angel <diaz.angeledu@gmail.com>
2020-10-19 10:12:28 -0400

    onToggleFavorite

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,29 @@ import "./App.css";
 import * as api from "../../api";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
+import Filter from "../Filter";
 import Tool from "../Tool";
 
 function App() {
   // Crear el estado
   const [tools, setTools] = useState(api.Tools.list());
   // Crear metodo que cambia el favorite
-  const onToggleFavorite = () => {
-    console.log(tools);
-    // setTools(tool.is_favorite ? false : true);
+  const onToggleFavorite = (id) => {
+    setTools(
+      tools.map((item) => {
+        if (item.id === id) {
+          let itemFavorite = item.is_favorite ? false : true;
+          item.is_favorite = itemFavorite;
+          return item;
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const showFavorites = () => {
+    setTools(tools.filter((item) => item.is_favorite));
   };
 
   let favoriteCount = 0;
@@ -27,11 +41,15 @@ function App() {
       <Sidebar />
       <main className="main">
         <div className="container">
+          <Filter showFavorites={showFavorites} />
           <p>Favorite count: {favoriteCount}</p>
           <ul className="tool__list">
             {tools.map((tool) => (
               <li key={tool.id} className="tool">
-                <Tool tool={tool} onToggleFavorite={onToggleFavorite} />
+                <Tool
+                  tool={tool}
+                  onToggleFavorite={() => onToggleFavorite(tool.id)}
+                />
               </li>
             ))}
           </ul>
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,35 +1,7 @@
 import React from "react";
 import "./Filter.css";
 
-function Filter() {
-  return (
-    <form className="filter">
-      <div className="form__group">
-        <label className="form__label">Date</label>
-        <select>
-          <option value="1">First Option</option>
-        </select>
-      </div>
-      <div className="form__group">
-        <label className="form__label">Most review</label>
-        <select>
-          <option value="1">First Option</option>
-        </select>
-      </div>
-      <div className="form__group">
-        <label className="form__label">Most favorite</label>
-        <select>
-          <option value="1">First Option</option>
-        </select>
-      </div>
-      <div className="form__group">
-        <label className="form__label">Search</label>
-        <select>
-          <option value="1">First Option</option>
-        </select>
-      </div>
-      <button>Submit</button>
-    </form>
-  );
+function Filter(props) {
+  return <button onClick={props.showFavorites}>Show Favorites</button>;
 }
 export default Filter;
